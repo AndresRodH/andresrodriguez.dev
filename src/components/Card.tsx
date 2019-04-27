9b1be16610b9aecc2097491f21bff659b04eaa3d
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2019-04-26 22:38:19 -0500

    add work in progress page

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled"
+
+const Card = styled.div`
+  border-radius: 8px;
+  box-shadow: 0 16px 24px rgba(0, 0, 0, 0.25), 0 8px 8px rgba(0, 0, 0, 0.22);
+  background-color: white;
+`
+
+export default Card
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,44 @@
 import React from "react"
+import styled from "@emotion/styled"
+import Card from "../components/Card"
+import { animated, useSpring, config } from "react-spring"
 
-const Default: React.FC = () => <div>Hello world!</div>
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: linear-gradient(to top, #37ecba 0%, #72afd3 100%);
+`
+
+const StyledCard = styled(Card)`
+  padding: 40px 56px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 48px;
+  font-weight: bold;
+  flex-direction: column;
+`
+
+const AnimatedCard = animated(StyledCard)
+
+const Default: React.FC = () => {
+  const props = useSpring({
+    opacity: 1,
+    config: config.slow,
+    from: { opacity: 0 },
+  })
+  return (
+    <Container>
+      <AnimatedCard style={props}>
+        <span style={{ flex: 1, paddingBottom: 16 }}>WORK IN PROGRESS</span>
+        <span role="img" aria-label="work-in-progress">
+          👨🏻‍🏭
+        </span>
+      </AnimatedCard>
+    </Container>
+  )
+}
 
 export default Default
