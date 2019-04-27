6fa76063a18a00cf545be24324a5b20a32561348
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2019-04-26 23:44:50 -0500

    use a layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,45 +1,14 @@
 import React from "react"
-import styled from "@emotion/styled"
-import Card from "../components/Card"
-import { animated, useSpring, config } from "react-spring"
 import { Link } from "gatsby"
+import BaseLayout from "../layouts/BaseLayout"
 
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background-image: linear-gradient(-60deg, #ff5858 0%, #f09819 100%);
-`
-
-const StyledCard = styled(Card)`
-  padding: 40px 56px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 48px;
-  font-weight: bold;
-  flex-direction: column;
-`
-
-const AnimatedCard = animated(StyledCard)
-
-const Default: React.FC = () => {
-  const props = useSpring({
-    opacity: 1,
-    config: config.slow,
-    from: { opacity: 0 },
-  })
+export default function NotFound() {
   return (
-    <Container>
-      <AnimatedCard style={props}>
-        NOT IMPLEMENTED
-        <Link style={{ fontSize: 16 }} to="/">
-          Take me back!
-        </Link>
-      </AnimatedCard>
-    </Container>
+    <BaseLayout>
+      NOT IMPLEMENTED
+      <Link style={{ fontSize: 16 }} to="/">
+        Take me back!
+      </Link>
+    </BaseLayout>
   )
 }
-
-export default Default
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,13 @@
 import React from "react"
-import styled from "@emotion/styled"
-import Card from "../components/Card"
-import { animated, useSpring, config } from "react-spring"
+import BaseLayout from "../layouts/BaseLayout"
 
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  background-image: linear-gradient(to top, #37ecba 0%, #72afd3 100%);
-`
-
-const StyledCard = styled(Card)`
-  padding: 40px 56px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 48px;
-  font-weight: bold;
-  flex-direction: column;
-`
-
-const AnimatedCard = animated(StyledCard)
-
-const Default: React.FC = () => {
-  const props = useSpring({
-    opacity: 1,
-    config: config.slow,
-    from: { opacity: 0 },
-  })
+export default function Default() {
   return (
-    <Container>
-      <AnimatedCard style={props}>
-        <span style={{ flex: 1, paddingBottom: 16 }}>WORK IN PROGRESS</span>
-        <span role="img" aria-label="work-in-progress">
-          👨🏻‍🏭
-        </span>
-      </AnimatedCard>
-    </Container>
+    <BaseLayout>
+      <span style={{ flex: 1, paddingBottom: 16 }}>WORK IN PROGRESS</span>
+      <span role="img" aria-label="work-in-progress">
+        👨🏻‍🏭
+      </span>
+    </BaseLayout>
   )
 }
-
-export default Default
