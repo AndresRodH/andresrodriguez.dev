16936a7271576d2755a8a55045dd3a903d490b2c
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2019-04-27 00:42:58 -0500

    add type-check script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import BaseLayout from "../layouts/BaseLayout"
 export default function Default() {
   return (
     <BaseLayout>
-      <span style={{ flex: 1, paddingBottom: 16 }}>WORK IN PROGRESS</span>
+      <span style={{ flex: 1, paddingBottom: 16 }}>WORK IN PROGRESS!</span>
       <span role="img" aria-label="work-in-progress">
         👨🏻‍🏭
       </span>
