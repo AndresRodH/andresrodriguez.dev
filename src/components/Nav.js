8055c0bc894d40017ddbc5606ea5396155cf2a3e
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2019-06-01 11:51:34 -0500

    wrap nav with <nav> element

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,17 +6,26 @@ import { rhythm } from "../utils/typography"
 
 function Nav() {
   return (
-    <Flex backgroundColor="palevioletred" alignItems="center">
-      <Logo
-        onClick={() => navigate("/")}
-        style={{
-          display: "flex",
-          cursor: "pointer",
-          height: rhythm(2.5),
-          width: rhythm(2.5),
-        }}
-      />
-    </Flex>
+    <nav>
+      <Flex
+        backgroundColor="palevioletred"
+        justifyContent="space-between"
+        alignItems="center"
+        p={`0 ${rhythm(0.5)} 0 0`}
+      >
+        <Logo
+          onClick={() => navigate("/")}
+          style={{
+            display: "flex",
+            cursor: "pointer",
+            height: rhythm(2.5),
+            width: rhythm(2.5),
+          }}
+        />
+
+        <Flex.Item justifySelf="flex-end">Hai</Flex.Item>
+      </Flex>
+    </nav>
   )
 }
 
