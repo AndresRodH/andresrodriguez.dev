6730c082187675ddefdb9b4da93a73f7abe60a49
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-01-06 09:24:04 -0600

    make git recognize case sensitive file changes

---
+++ src/components/Layout.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { css } from '@emotion/core'
+import { useStaticQuery, Link, graphql } from 'gatsby'
+import { rhythm } from '../utils/typography'
+
+export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `,
+  )
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link to={`/`}>
+        <span
+          css={css`
+            color: black;
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </span>
+      </Link>
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+      {children}
+    </div>
+  )
+}
---
+++ src/components/SEO.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { useStaticQuery, graphql } from 'gatsby'
+
+function SEO({ description, lang, meta, title, slug }) {
+  const {
+    site: { siteMetadata },
+  } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            image
+            siteUrl
+            social {
+              twitter
+            }
+          }
+        }
+      }
+    `,
+  )
+
+  const metaDescription = description || siteMetadata.description
+  const url = `${siteMetadata.siteUrl}${slug}`
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      {...(title
+        ? {
+            titleTemplate: `%s — ${siteMetadata.title}`,
+            title,
+          }
+        : {
+            title: `${siteMetadata.title}`,
+          })}
+      meta={[
+        {
+          name: 'description',
+          content: metaDescription,
+        },
+        {
+          name: 'image',
+          content: siteMetadata.image,
+        },
+        {
+          property: 'og:image',
+          content: siteMetadata.image,
+        },
+        {
+          property: 'og:url',
+          content: url,
+        },
+        {
+          property: 'og:title',
+          content: title || siteMetadata.title,
+        },
+        {
+          property: 'og:site_name',
+          content: 'Andrés Rodríguez',
+        },
+        {
+          property: 'og:description',
+          content: metaDescription,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          name: 'twitter:creator',
+          content: siteMetadata.social.twitter,
+        },
+        {
+          name: 'twitter:title',
+          content: title || siteMetadata.title,
+        },
+        {
+          name: 'twitter:description',
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  )
+}
+
+SEO.defaultProps = {
+  lang: 'en',
+  meta: [],
+  description: '',
+  slug: '',
+  title: '',
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string,
+  slug: PropTypes.string,
+}
+
+export default SEO
