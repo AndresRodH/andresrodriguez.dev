a6a043656add58010050a257760fe96a65a3525a
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-10-29 10:52:00 -0500

    use next/image component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react'
 import twin from 'twin.macro'
 import config from 'config'
+import Image from 'next/image'
 
 const A = twin.a`underline font-bold text-blue-500 border-blue-500`
+const {description} = config
 
 export function Footer() {
-  const {image, description, siteUrl} = config
-
   return (
     <footer className="w-full px-4 py-10">
       <div className="container mx-auto flex lg:justify-center items-center">
-        <img
-          className="rounded-full h-20 w-20"
-          src={`${siteUrl}/${image}`}
+        <Image
+          className="rounded-full object-cover"
+          height={80}
+          width={80}
+          src="/images/me.jpg"
           alt="Andrés Rodríguez"
         />
         <div className="ml-4">
