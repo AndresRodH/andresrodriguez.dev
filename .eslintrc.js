7a425fa9f076be4a1419cbac458e096aa1930de7
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-10-29 08:16:20 -0500

    warn on prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
     'plugin:prettier/recommended', // Make sure this is always the last element in the array.
   ],
   rules: {
-    'prettier/prettier': ['error', {}, {usePrettierrc: true}],
+    'prettier/prettier': ['warn', {}, {usePrettierrc: true}],
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
