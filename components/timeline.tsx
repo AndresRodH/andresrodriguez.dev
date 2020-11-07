b96032a5477710e999a8100ccf5f22a1206f7916
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-11-07 07:43:45 -0600

    fix: colors not showing on timeline svgs in webkit

--- components/timeline.tsx
+++ components/timeline.tsx
@@ -20,12 +20,16 @@ function Entry({
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
-          fill="currentColor"
           className="flex h-6 w-6"
         >
-          <circle cx="12" cy="12" r="10" className="text-teal-400" />
+          <circle
+            cx="12"
+            cy="12"
+            r="10"
+            className="fill-current text-teal-400"
+          />
           <path
-            className="text-teal-800"
+            className="fill-current text-teal-800"
             d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z"
           />
         </svg>
