ca07b8cb7622a2e3868a287c9a23a94f2775ccfd
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-11-08 09:44:52 -0600

    cleanup some css and center navlinks

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <header>
       <div className="w-full max-w-screen-lg mx-auto px-4 lg:px-8 py-10 flex justify-between items-center">
         <Logo />
-        <div className="grid grid-rows-1 gap-4 grid-cols-2">
+        <div className="grid grid-rows-1 gap-4 text-center grid-cols-2">
           <Link href="/about">About</Link>
           <Link href="/uses">Uses</Link>
         </div>
