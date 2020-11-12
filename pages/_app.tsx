8d2f0f75c811cdff6c30122d05c6ab7fc9bc1eec
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-11-12 11:19:29 -0600

    feat: add react query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,20 @@ import type {AppProps} from 'next/app'
 import {GlobalStyles} from 'twin.macro'
 import {DefaultSeo} from 'next-seo'
 import {seo} from 'config'
+import {ReactQueryDevtools} from 'react-query-devtools'
+import {QueryCache, ReactQueryCacheProvider} from 'react-query'
+
+const queryCache = new QueryCache()
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
       <GlobalStyles />
       <DefaultSeo {...seo} />
-      <Component {...pageProps} />
+      <ReactQueryCacheProvider queryCache={queryCache}>
+        <ReactQueryDevtools initialIsOpen />
+        <Component {...pageProps} />
+      </ReactQueryCacheProvider>
     </>
   )
 }
