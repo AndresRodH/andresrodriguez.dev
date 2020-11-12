842027f9198bc3556f89664277b2a5d65bb37b70
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-11-12 14:26:21 -0600

    feat post-item component

---
+++ components/post-item.tsx
@@ -0,0 +1,27 @@
+import {GetSortedPostsData} from 'lib/posts'
+import {ViewCounter} from './view-counter'
+import tw, {styled} from 'twin.macro'
+
+const PostItemWrapper = styled.div`
+  grid-template-areas:
+    'title views'
+    'description description';
+  ${tw`prose cursor-pointer gap-2 py-4 flex flex-col sm:grid`};
+`
+
+export function PostItem({id, title, description}: GetSortedPostsData) {
+  return (
+    <PostItemWrapper>
+      <span
+        style={{gridArea: 'title'}}
+        className="text-xl sm:text-2xl text-gray-900 font-bold leading-tight"
+      >
+        {title}
+      </span>
+      <ViewCounter style={{gridArea: 'views'}} id={id} />
+      <p style={{gridArea: 'description'}} tw="mt-0!">
+        {description}
+      </p>
+    </PostItemWrapper>
+  )
+}
