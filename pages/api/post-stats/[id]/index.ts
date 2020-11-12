88e900db13f082d93b0884aeb60bbbefd5f685fb
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-11-12 11:17:18 -0600

    feat: add api routes for page stats

---
+++ pages/api/post-stats/[id]/index.ts
@@ -0,0 +1,37 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+import {db} from 'lib/firebase-admin'
+
+export type PostStats = {
+  views: number
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.query.id)
+    return res.status(400).json({
+      error:
+        'Query parameter "id" is missing from the request, did you forget to include it?',
+    })
+
+  if (Array.isArray(req.query.id))
+    return res.status(400).json({
+      error: 'Multiple ids are not currently supported',
+    })
+
+  const postStatsRef = db.collection('posts').doc(req.query.id)
+
+  const postStats = await db.runTransaction<PostStats>(async (t) => {
+    const snapshot = await t.get(postStatsRef)
+    let postStats: PostStats
+
+    if (!snapshot.exists) {
+      postStats = {
+        views: 0,
+      }
+      t.set(postStatsRef, postStats)
+    } else postStats = snapshot.data() as PostStats
+
+    return postStats
+  })
+
+  return res.status(200).json(postStats)
+}
