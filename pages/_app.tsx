5ceb4020b81af0efbf5816db5f24eb9667b576e8
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-11-18 07:57:51 -0600

    feat: favicon + new logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'typeface-inter'
 import 'styles/index.css'
 import type {AppProps} from 'next/app'
+import Head from 'next/head'
 import {GlobalStyles} from 'twin.macro'
 import {DefaultSeo} from 'next-seo'
 import {seo} from 'config'
@@ -12,6 +13,28 @@ const queryCache = new QueryCache()
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <GlobalStyles />
       <DefaultSeo {...seo} />
       <ReactQueryCacheProvider queryCache={queryCache}>
