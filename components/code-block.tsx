98c63d981d77a9c0717a41f1ad1ad3a606de9654
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-11-22 09:49:13 -0600

    fix overflowing code blocks

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Highlight, {defaultProps, Language} from 'prism-react-renderer'
 import nightOwl from 'prism-react-renderer/themes/nightOwl'
+import {styled, theme} from 'twin.macro'
+
+const Pre = styled.pre`
+  max-width: 50ch;
+  @media (min-width: ${theme`screens.sm`}) {
+    max-width: 100%;
+  }
+`
 
 export function CodeBlock({
   children,
@@ -10,7 +18,7 @@ export function CodeBlock({
   className: string
 }) {
   const language = className.replace(/language-/, '') as Language
-
+  console.log(theme`screens.sm`)
   return (
     <Highlight
       {...defaultProps}
@@ -19,7 +27,7 @@ export function CodeBlock({
       theme={nightOwl}
     >
       {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style, padding: '20px'}}>
+        <Pre className={className} style={style}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({line, key: i})}>
               {line.map((token, key) => (
@@ -27,7 +35,7 @@ export function CodeBlock({
               ))}
             </div>
           ))}
-        </pre>
+        </Pre>
       )}
     </Highlight>
   )
