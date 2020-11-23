d3a5c84002fba2236b600e039dbdfe2dece5fe7e
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-11-23 09:45:44 -0600

    feat: add active styles to nav

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,5 +1,24 @@
+import {useRouter} from 'next/router'
 import Link from 'next/link'
 import {Logo} from './logo'
+import clsx from 'clsx'
+
+function NavLink({to, children}: {to: string; children: React.ReactNode}) {
+  const router = useRouter()
+
+  return (
+    <Link href={to}>
+      <span
+        className={clsx(
+          'cursor-pointer',
+          router.pathname === to && 'font-semibold text-teal-600',
+        )}
+      >
+        {children}
+      </span>
+    </Link>
+  )
+}
 
 export function Nav() {
   return (
@@ -7,8 +26,8 @@ export function Nav() {
       <div className="w-full max-w-screen-lg mx-auto px-4 lg:px-8 py-10 flex justify-between items-center">
         <Logo />
         <div className="grid grid-rows-1 gap-4 text-center grid-cols-2">
-          <Link href="/about">About</Link>
-          <Link href="/uses">Uses</Link>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/uses">Uses</NavLink>
         </div>
       </div>
     </nav>
