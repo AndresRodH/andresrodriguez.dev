bbc075a112baad43c2e97acb97568dc04407d98c
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-12-10 15:32:35 -0600

    add XStateViz component

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,8 +1,18 @@
 /* eslint-disable react/display-name */
 import {Components} from '@mdx-js/react'
 import {CodeBlock} from './code-block'
+import {XStateViz} from './xstate-viz'
 
-export const components: Components = {
+const domComponents: Components = {
   pre: (props) => <div {...props} />,
   code: CodeBlock,
+}
+
+const customComponents = {
+  XStateViz,
+}
+
+export const components = {
+  ...domComponents,
+  ...customComponents,
 }
