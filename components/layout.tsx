d5eef3cee976c4a154bd435288b31072e27d065b
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2021-01-17 08:47:52 -0600

    remove grid from layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,13 +3,10 @@ import {Nav} from 'components/nav'
 
 export function Layout({children}) {
   return (
-    <div
-      className="grid min-h-screen"
-      style={{gridTemplateColumns: '1fr', gridTemplateRows: 'auto 1fr auto'}}
-    >
+    <>
       <Nav />
       {children}
       <Footer />
-    </div>
+    </>
   )
 }
