0eeed7a36f81cc49bc19e4bb780074db92b414a6
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2021-01-17 08:51:20 -0600

    new post item UI

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -1,27 +1,25 @@
+import {format, parseISO} from 'date-fns'
 import {GetSortedPostsData} from 'lib/posts'
-import {ViewCounter} from './view-counter'
-import tw, {styled} from 'twin.macro'
+import Link from 'next/link'
 
-const PostItemWrapper = styled.div`
-  grid-template-areas:
-    'title views'
-    'description description';
-  ${tw`prose cursor-pointer gap-2 py-4 flex flex-col sm:grid`};
-`
-
-export function PostItem({id, title, description}: GetSortedPostsData) {
+export function PostItem({id, title, description, date}: GetSortedPostsData) {
   return (
-    <PostItemWrapper>
-      <span
-        style={{gridArea: 'title'}}
-        className="text-xl sm:text-2xl text-gray-900 font-bold leading-tight"
-      >
-        {title}
-      </span>
-      <ViewCounter className="ml-auto" style={{gridArea: 'views'}} id={id} />
-      <p style={{gridArea: 'description'}} tw="mt-0!">
-        {description}
-      </p>
-    </PostItemWrapper>
+    <div className="py-8">
+      <div className="mb-4">
+        <div className="space-y-1 text-xs font-semibold tracking-wide uppercase">
+          <p className="text-gray-600">
+            {format(parseISO(date), 'LLLL d, yyyy')}
+          </p>
+        </div>
+      </div>
+      <div className="sm:col-span-3 lg:col-span-2">
+        <Link key={id} href={`/blog/${id}`}>
+          <a className="mb-3 inline-block text-gray-900 transition-colors duration-200 hover:text-indigo-500 text-3xl font-extrabold leading-none sm:text-4xl xl:text-4xl">
+            {title}
+          </a>
+        </Link>
+        <p className="text-gray-700">{description}</p>
+      </div>
+    </div>
   )
 }
